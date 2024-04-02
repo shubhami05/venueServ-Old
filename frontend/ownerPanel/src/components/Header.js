f4229985e987dff2a369b952b4193c960aed2d15
shubham italiya <shubhamitaliya05@gmail.com>
2024-04-02 20:23:54 +0530

    .

--- frontend/ownerPanel/src/components/Header.js
+++ frontend/ownerPanel/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {  } from 'react'
 
 function Header() {
     const handleLogout = () => {
@@ -10,7 +10,7 @@ function Header() {
         <>
             <nav className="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
                 <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-                    <a className="nav-item nav-link px-0 me-xl-4" href="">
+                    <a className="nav-item nav-link px-0 me-xl-4" href="/">
                         <i className="bx bx-menu bx-sm" />
                     </a>
                 </div>
